4616c9afd75153d2d81583946f4ce707772ebcfe
ToniIvars <toniivaju@gmail.com>
2022-09-08 18:32:47 +0200

    Add landing page and favicon

--- authentication/static/auth/base.js
+++ authentication/static/auth/base.js
@@ -1,5 +1,5 @@
 const inputs = document.querySelectorAll('input:required')
 inputs.forEach(inp => {
-    const label = inp.previousElementSibling
-    label.classList.toggle('required')
+  const label = inp.previousElementSibling
+  label.classList.toggle('required')
 })
--- traffiwork/urls.py
+++ traffiwork/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from .views import landing
+
 urlpatterns = [
+    path('', landing, name='landing'),
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('students/', include('student_profile.urls')),
