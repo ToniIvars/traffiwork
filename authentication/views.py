5134aa30396bfc1a4a4f48a32f09c8893db8a026
ToniIvars <toniivaju@gmail.com>
2022-09-09 11:53:33 +0200

    Remove unused import

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,5 @@
 from django.contrib import messages
 from django.contrib.auth import authenticate, login
-from django.contrib.auth.models import User
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
